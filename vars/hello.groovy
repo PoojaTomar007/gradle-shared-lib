7c7ee574b9fb36329892969ba6490400bd2e0c74
PoojaTomar007 <90127609+PoojaTomar007@users.noreply.github.com>
2021-09-12 13:21:17 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-    echo "Hello, ${name}."
-}
+def sout = new StringBuilder(), serr = new StringBuilder()
+def proc = '/opt/gradle/gradle-6.4.1/bin/gradle -v'.execute()
+proc.consumeProcessOutput(sout, serr)
+proc.waitForOrKill(1000)
+println "out> $sout\nerr> $serr"
